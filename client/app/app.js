48690e1dd88b39115070df202b864f7f838d094f
Verrol L. Adams <vla@lorrev.org>
2015-08-06 10:53:02 -0400

    main.html using md-button, md-input, and md-silier

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,9 @@ angular.module('angularMaterialApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+    'ngAnimate',
+    'ngMaterial'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
